299415a1d4024187553fd13da2b706d75ff0a9e3
sangwoong <timeattacks2@naver.com>
2023-11-13 14:15:19 +0900

    FEAT : product 조회 컴포넌트 수정

--- production-management/src/pages/Product/ViewProducts.tsx
+++ production-management/src/pages/Product/ViewProducts.tsx
@@ -1,13 +1,36 @@
+import {Box} from "@material-ui/core";
 import React, {Component} from "react";
-import ViewTable from "../../components/Product/ViewTable";
+import ViewProductListTable from "../../components/Product/ViewProductListTable";
 import ProductSearchBar from "../../components/Product/ProductSearchBar";
 
 class ViewProducts extends Component {
   render() {
     return (
         <>
+          <Box
+              sx={{
+                width: '125vh',
+                height: '1.5vh',
+                ml: '10vh', // 왼쪽 마진
+                mt: '5vh', // 상단 마진
+                pt: '1vh',
+                p: '1.5vh',
+                border: '1px solid #D3D3D3',
+              }}
+          >
+            <span style={{fontSize: '1.7vh', fontWeight: 'bold'}}>지시현황</span>
+          </Box>
+          <Box
+              sx={{
+                width: '125vh',
+                ml: '10vh', // 왼쪽 마진
+                p: '1.5vh',
+                border: '1px solid #D3D3D3'
+              }}
+          >
           <ProductSearchBar/>
-          <ViewTable/>
+          {/*<ViewProductListTable/>*/}
+          </Box>
         </>
     )
   }
