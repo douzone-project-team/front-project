c7b48a5c21a7b1cedceaf1af6819bba39bf150af
sangwoong <timeattacks2@naver.com>
2023-11-13 14:26:16 +0900

    FEAT : App, index instruction add, list 추가

--- production-management/src/App.tsx
+++ production-management/src/App.tsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ViewProducts from "./pages/Product/ViewProducts";
+import ViewInstructions from "./pages/Instruction/ViewInstructions";
+import AddInstructions from "./pages/Instruction/AddInstructions";
 
 class App extends Component {
   render() {
@@ -8,6 +10,8 @@ class App extends Component {
         <Router>
           <Switch>
             <Route exact path="/product/list" component={ViewProducts}/>
+            <Route exact path="/instruction/list" component={ViewInstructions}/>
+            <Route exact path="/instruction/add" component={AddInstructions}/>
           </Switch>
         </Router>
     );
