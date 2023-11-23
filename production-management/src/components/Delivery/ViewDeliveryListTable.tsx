9eab167bb6269ad0c2028b64bae6c5c1168f6d34
OhSoomin812 <o3o_121@naver.com>
2023-11-23 11:19:47 +0900

    FEAT : delivery 수정, 삭제 기능 추가

--- production-management/src/components/Delivery/ViewDeliveryListTable.tsx
+++ production-management/src/components/Delivery/ViewDeliveryListTable.tsx
@@ -25,12 +25,16 @@ const cellStyle = {
     width: '10%',
 };
 
-class ViewDeliveryListTable extends Component {
+type Props = {
+    tableSize: boolean
+}
+
+class ViewDeliveryListTable extends Component<Props>{
     static contextType = DeliveriesContext;
 
     render() {
         const state = this.context as DeliveriesState;
-        const list = state.deliveryPage.deliveries || [];
+        const list = state.deliveryPage.list || [];
 
         const handleNextPage = () => {
             if (state.deliveryPage.hasNextPage) {
@@ -47,23 +51,28 @@ class ViewDeliveryListTable extends Component {
         return (
             <>
                 <span className='table-header'>출고 목록</span>
-                <TableContainer className='table-container'>
+                <TableContainer className='table-container' style={{
+                    height: this.props.tableSize ? '330px' : '90px',
+                    transition: 'height 0.3s ease-in-out'
+                }}>
                     <Table size='small' className='table'>
                         <TableHead>
                             <TableRow>
                                 <TableCell align="center" style={boldCellStyle}>출고 상태</TableCell>
                                 <TableCell align="center" style={boldCellStyle}>출고 번호</TableCell>
                                 <TableCell align="center" style={boldCellStyle}>담당자</TableCell>
-                                <TableCell align="center" style={boldCellStyle}>지시일</TableCell>
+                                <TableCell align="center" style={boldCellStyle}>출고일</TableCell>
                                 <TableCell align="center" style={boldCellStyle}>지시 개수</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {list.map((row) => (
-                                <TableRow key={row.deliveryNo}>
-                                    <TableCell align="center" style={cellStyle}>{row.deliveryStatus}</TableCell>
-                                    <TableCell align="center" style={cellStyle}
-                                               onClick={() => state.getDelivery(row.deliveryNo)}>{row.deliveryNo}</TableCell>
+                            {list && list.length > 0 && list.map((row) => (
+                                <TableRow key={row.deliveryNo} className='cellHoverEffect'
+                                            onClick={() => state.getDelivery(row.deliveryNo)}>
+                                    <TableCell align="center" style={cellStyle}>
+                                        {row.deliveryStatus === 'COMPLETE' ? '완료' : '미완료'}
+                                    </TableCell>
+                                    <TableCell align="center" style={cellStyle}>{row.deliveryNo}</TableCell>
                                     <TableCell align="center" style={cellStyle}>{row.employeeName}</TableCell>
                                     <TableCell align="center" style={cellStyle}>{row.deliveryDate}</TableCell>
                                     <TableCell align="center" style={cellStyle}>{row.instructionCount}</TableCell>
@@ -78,16 +87,8 @@ class ViewDeliveryListTable extends Component {
                             alignItems: 'center',
                         }}
                     >
-                        <KeyboardArrowLeft
-                            onClick={handlePrevPage}
-                        >
-                            이전 페이지
-                        </KeyboardArrowLeft>
-                        <KeyboardArrowRight
-                            onClick={handleNextPage}
-                        >
-                            다음 페이지
-                        </KeyboardArrowRight>
+                        <KeyboardArrowLeft onClick={handlePrevPage} />
+                        <KeyboardArrowRight onClick={handleNextPage} />
                     </Box>
                 </TableContainer>
             </>
