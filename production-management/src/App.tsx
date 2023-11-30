13c0b318b8357e35bfd1f9aaab649ccb2b6731ae
geun-7 <worms502@gmail.com>
2023-11-30 18:06:49 +0900

    REFACTOR: App.tsx route수정

--- production-management/src/App.tsx
+++ production-management/src/App.tsx
@@ -8,6 +8,11 @@ import MainPage from './pages/Main/MainPage';
 import Login from './pages/Auth/Login';
 import ViewDeliveries from './pages/Delivery/ViewDeliveries';
 import AddDeliveries from './pages/Delivery/AddDeliveries';
+import Mypage from "./pages/Employee/Mypage";
+import ViewEmployees from "./pages/Auth/ViewEmployees";
+import AddEmployees from "./pages/Auth/AddEmployees";
+import Error500 from "./pages/Error/Error500";
+import Error404 from "./pages/Error/Error404";
 
 class App extends Component {
   render() {
@@ -16,12 +21,17 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={MainPage}/>
             <Route exact path="/login" component={Login}/>
+            <Route exact path="/myPage" component={Mypage}/>
             <Route exact path="/product/list" component={ViewProducts}/>
             <Route exact path="/instruction/list" component={ViewInstructions}/>
             <Route exact path="/instruction/add" component={AddInstructions}/>
             <Route exact path="/customer/list" component={CustomerPage}/>
             <Route exact path="/delivery/list" component={ViewDeliveries}/>
             <Route exact path="/delivery/add" component={AddDeliveries}/>
+            <Route exact path="/employee/list" component={ViewEmployees}/>
+            <Route exact path="/employee/add" component={AddEmployees}/>
+            <Route exact path="/error" component={Error500}/>
+            <Route exact path="/not-found" component={Error404}/>
           </Switch>
         </Router>
     );
