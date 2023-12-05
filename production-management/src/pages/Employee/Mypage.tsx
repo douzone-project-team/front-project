2308a78fbd71408faee16ecce18cf30291a4b2e4
OhSoomin812 <o3o_121@naver.com>
2023-12-05 10:51:19 +0900

    FEAT: 마이페이지 myInstruction, myDelivery 기능 추가

--- production-management/src/pages/Employee/Mypage.tsx
+++ production-management/src/pages/Employee/Mypage.tsx
@@ -1,8 +1,11 @@
-import {Component} from "react";
+import React, {Component} from "react";
 import ProfileForm from "../../components/Employee/ProfileForm";
 import Layout from "../../common/Layout";
-import {Box} from "@material-ui/core";
+import {Box, Grid} from "@material-ui/core";
 import ProfileImage from "../../components/Employee/ProfileImage";
+import MyInstruction from "../../components/Employee/MyInstruction";
+import MyDelivery from "../../components/Employee/MyDelivery";
+import Typography from "@material-ui/core/Typography";
 
 class Mypage extends Component {
 
@@ -10,14 +13,42 @@ class Mypage extends Component {
         return (
             <Layout>
                 <Box
+                    sx={{
+                        width: '100%',
+                        mt: '60px',
+                        mb: '20px',
+                        pt: '20px',
+                        pl: '15px',
+                        pb: '15px',
+                        bgcolor: '#3C50C2',
+                        color: 'white'
+                    }}
+                >
+                    <span style={{fontSize: '17px', fontWeight: 'bold'}}>마이 페이지</span>
+                </Box>
+                <Box>
+                <Box
                     style={{
                         display: 'flex',
                         justifyContent: 'center',
-                        marginTop: '110px',
+                        marginTop: '40px',
                     }}
                 >
-                    <ProfileImage />
-                    <ProfileForm />
+                    <Grid style={{ marginRight: '20px' }}>
+                        <ProfileImage/>
+                    </Grid>
+                    <ProfileForm/>
+                </Box>
+                <Box
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        marginTop: '50px'
+                    }}
+                >
+                    <MyInstruction />
+                    <MyDelivery/>
+                </Box>
                 </Box>
             </Layout>
         )
