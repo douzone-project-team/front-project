5f11c7867e3300f213dbadec76bab3ac0258861f
geun-7 <worms502@gmail.com>
2023-12-10 20:14:26 +0900

    REFACTOR: Customer 등록, 수정Modal 코드 리팩토링

--- production-management/src/components/Modal/Customer/CustomerModifyModal.tsx
+++ production-management/src/components/Modal/Customer/CustomerModifyModal.tsx
@@ -3,6 +3,7 @@ import {CustomersState, InsertCustomer} from "../../../object/Customer/customer-
 import {CustomersContext} from "../../../store/Customer/customers-context";
 import {Box} from "@material-ui/core";
 import './CustomerModalCss.css'
+import BusinessIcon from "@material-ui/icons/Business";
 
 type CustomerModalProps = {
     onClose: () => void,
@@ -76,120 +77,125 @@ export class CustomerModifyModal extends Component<CustomerModalProps, CustomerM
         modifyValue.ceo = customer.ceo;
         return (
             <div className='modal'>
-                {status ? (
-                    <section className='modal-container' style={{ display: 'grid', gridTemplateRows: 'auto 1fr auto', width:'370px'}}>
-                        <header>
-                            <button className="close" onClick={onClose}>
-                                &times;
-                            </button>
-                        </header>
-                        <main style={{border: "none", display: 'grid', placeItems: 'center'}}>
-                            <Box
-                                sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    padding: '20px',
-                                    border: '1px solid #ccc',
-                                    borderRadius: '8px',
-                                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)'
+                <section className='modal-container' style={{ display: 'grid', gridTemplateRows: 'auto 1fr auto', width:'450px', height: '600px'}}>
+                    <div className="modalHeader" style={{height: '55px'}}>
+                        <div style={{display: 'flex'}}><BusinessIcon/>&nbsp;거래처 수정</div>
+                        <button className="close" onClick={onClose}>
+                            &times;
+                        </button>
+                    </div>
+                    <main className="customer-modal-main" style={{border: "none", display: 'grid', placeItems: 'center'}}>
+                        <Box
+                            sx={{
+                                width : '80%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                borderRadius: '8px',
+                            }}
+                        >
+                            <label className="form-label">
+                                거래처 번호
+                            </label>
+                            <input
+                                type="text"
+                                placeholder=""
+                                className="form-input"
+                                value={customer.customerNo}
+                                disabled
+                            />
+                            <label className="form-label">
+                                거래처 코드
+                            </label>
+                            <div style={{display:'flex'}}>
+                                <input
+                                    type="text"
+                                    placeholder="ex) C0001"
+                                    className="form-input"
+                                    value={customer.customerCode}
+                                    disabled
+                                />
+                            </div>
+                            <label className="form-label">
+                                거래처 명칭
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="ex) 더존비즈온"
+                                className="form-input"
+                                defaultValue={customer.customerName}
+                                onChange={event => {
+                                    modifyValue.customerName = event.target.value;
                                 }}
-                            >
-                                <label className="form-label">
-                                    거래처 번호
-                                    <input
-                                        type="text"
-                                        placeholder=""
-                                        className="form-input"
-                                        value={customer.customerNo}
-                                        disabled
-                                    />
-                                </label>
-                                <label className="form-label">
-                                    거래처 코드
-                                    <input
-                                        type="text"
-                                        placeholder="ex) C0001"
-                                        className="form-input"
-                                        value={customer.customerCode}
-                                        disabled
-                                    />
-                                </label>
-                                <label className="form-label">
-                                    거래처 명칭
-                                    <input
-                                        type="text"
-                                        placeholder="ex) 더존비즈온"
-                                        className="form-input"
-                                        defaultValue={customer.customerName}
-                                        onChange={event => {
-                                            modifyValue.customerName = event.target.value;
-                                        }}
-                                    />
-                                </label>
-                                <label className="form-label">
-                                    회사 연락처
-                                    <input
-                                        type="text"
-                                        placeholder="010"
-                                        className="form-input"
-                                        style={{width:'40px'}}
-                                        defaultValue={customerTelArray[0]}
-                                        onChange={event => {
-                                            modifyValue.customerTel1 = event.target.value;
-                                        }}
-                                    /> -
-                                    <input
-                                        type="text"
-                                        placeholder="0000"
-                                        className="form-input"
-                                        style={{width:'45px', marginLeft:'7px'}}
-                                        defaultValue={customerTelArray[1]}
-                                        onChange={event => {
-                                            modifyValue.customerTel2 = event.target.value;
-                                        }}
-                                    /> -
-                                    <input
-                                        type="text"
-                                        placeholder="0000"
-                                        className="form-input"
-                                        style={{width:'45px', marginLeft:'7px'}}
-                                        defaultValue={customerTelArray[2]}
-                                        onChange={event => {
-                                            modifyValue.customerTel3 = event.target.value;
-                                        }}
-                                    />
-                                </label>
-                                <label className="form-label">
-                                    회사 대표자
-                                    <input
-                                        type="text"
-                                        placeholder="ex) 김아무개"
-                                        className="form-input"
-                                        defaultValue={customer.ceo}
-                                        onChange={event => {
-                                            modifyValue.ceo = event.target.value;
-                                        }}
-                                    />
-                                </label>
-                                <label className="form-label">
-                                    거래처 업종
-                                    <input
-                                        type="text"
-                                        placeholder="ex) 제조업"
-                                        className="form-input"
-                                        value={customer.sector}
-                                        disabled
-                                    />
-                                </label>
+                            />
+                            <label className="form-label">
+                                회사 연락처
+                            </label>
+                            <div style={{display: 'flex', alignItems: 'center', marginBottom: '10px'}}>
+                                <input
+                                    type="text"
+                                    placeholder="010"
+                                    className="form-input"
+                                    style={{width:'15%', marginBottom:0, textAlign: 'center'}}
+                                    defaultValue={customerTelArray[0]}
+                                    onChange={event => {
+                                        modifyValue.customerTel1 = event.target.value;
+                                    }}
+                                /> &nbsp;-&nbsp;
+                                <input
+                                    type="text"
+                                    placeholder="0000"
+                                    className="form-input"
+                                    style={{width:'15%', marginLeft:'3px', marginBottom:0, textAlign: 'center'}}
+                                    defaultValue={customerTelArray[1]}
+                                    onChange={event => {
+                                        modifyValue.customerTel2 = event.target.value;
+                                    }}
+                                /> &nbsp;-&nbsp;
+                                <input
+                                    type="text"
+                                    placeholder="0000"
+                                    className="form-input"
+                                    style={{width:'15%',  marginLeft:'3px', marginBottom:0, textAlign: 'center'}}
+                                    defaultValue={customerTelArray[2]}
+                                    onChange={event => {
+                                        modifyValue.customerTel3 = event.target.value;
+                                    }}
+                                />
+                            </div>
+                            <label className="form-label">
+                                회사 대표자
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="ex) 김아무개"
+                                className="form-input"
+                                defaultValue={customer.ceo}
+                                onChange={event => {
+                                    modifyValue.ceo = event.target.value;
+                                }}
+                            />
+                            <label className="form-label">
+                                거래처 업종
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="ex) 제조업"
+                                className="form-input"
+                                value={customer.sector}
+                                disabled
+                            />
+                            <div style={{display : 'flex', justifyContent: 'center'}}>
+                                <button className="form-cancel-button" style={{border: '1px solid lightgray'}} onClick={onClose}>
+                                    취소
+                                </button>
+                                &nbsp;
                                 <button className="form-button" onClick={this.state.updateCustomer}>
                                     수정
                                 </button>
-                            </Box>
-                        </main>
-                        <footer style={{padding: '30px'}}>
-                        </footer>
-                    </section>
-                ) : null}
+                            </div>
+                        </Box>
+                    </main>
+                </section>
             </div>
         )
     }
