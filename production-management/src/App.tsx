4b568e1598d770b647237a69c5ed0d988d92c472
sangwoong <timeattacks2@naver.com>
2023-12-11 14:45:39 +0900

    FEAT : 존재하지 않는 url 접근 제한

--- production-management/src/App.tsx
+++ production-management/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import ViewProducts from "./pages/Product/ViewProducts";
 import ViewInstructions from "./pages/Instruction/ViewInstructions";
 import AddInstructions from "./pages/Instruction/AddInstructions";
@@ -13,24 +13,56 @@ import Mypage from "./pages/Employee/Mypage";
 import ViewEmployees from "./pages/Auth/ViewEmployees";
 import Error500 from "./pages/Error/Error500";
 import Error404 from "./pages/Error/Error404";
+import Swal from 'sweetalert2';
 
+type PrivateRouteProps = {
+  component: React.ComponentType<any>;
+  path: string
+}
+
+export class PrivateRoute extends Component<PrivateRouteProps> {
+  redirectLoginPage = () => {
+    Swal.fire({
+      icon: "warning",
+      text: "로그인 이후 접근 가능합니다.",
+    });
+    return <Redirect to="/login"/>;
+  }
+
+
+  render() {
+    const {component: Component, path} = this.props;
+
+    return (
+        <Route
+            render={(props) =>
+                localStorage.getItem('accessToken') !== null ? (
+                    <Component exact path={path}/>
+                ) : (
+                    this.redirectLoginPage()
+                )
+            }
+        />
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
         <Router>
           <Switch>
-            <Route exact path="/" component={MainPage}/>
+            <PrivateRoute path="/main-page" component={MainPage}/>
+            <PrivateRoute path="/my-page" component={Mypage}/>
+            <PrivateRoute path="/product/list" component={ViewProducts}/>
+            <PrivateRoute path="/instruction/list" component={ViewInstructions}/>
+            <PrivateRoute path="/instruction/add" component={AddInstructions}/>
+            <PrivateRoute path="/customer/list" component={CustomerPage}/>
+            <PrivateRoute path="/delivery/list" component={ViewDeliveries}/>
+            <PrivateRoute path="/delivery/add" component={AddDeliveries}/>
+            <PrivateRoute path="/current/page" component={currentSituationpage}/>
+            <PrivateRoute path="/employee/list" component={ViewEmployees}/>
             <Route exact path="/login" component={Login}/>
-            <Route exact path="/my-page" component={Mypage}/>
-            <Route exact path="/product/list" component={ViewProducts}/>
-            <Route exact path="/instruction/list" component={ViewInstructions}/>
-            <Route exact path="/instruction/add" component={AddInstructions}/>
-            <Route exact path="/customer/list" component={CustomerPage}/>
-            <Route exact path="/delivery/list" component={ViewDeliveries}/>
-            <Route exact path="/delivery/add" component={AddDeliveries}/>
-            <Route exact path="/current/page" component={currentSituationpage}/>
-            <Route exact path="/employee/list" component={ViewEmployees}/>
             <Route exact path="/error" component={Error500}/>
             <Route exact path="/not-found" component={Error404}/>
             <Route exact path="/*" component={Error404}/>
