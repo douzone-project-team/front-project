c160d2ebd59f3b79db9ade3f68893f42a70fe455
OhSoomin812 <o3o_121@naver.com>
2023-12-13 11:26:33 +0900

    FEAT: 이미지 드래그 업로드 기능 추가

--- production-management/src/components/Employee/ProfileImage.tsx
+++ production-management/src/components/Employee/ProfileImage.tsx
@@ -2,9 +2,11 @@ import React, {ChangeEvent, Component} from "react";
 import {EmployeeContext} from "../../store/Employee/employee-context";
 import {EmployeeState} from "../../object/Employee/employee-object";
 import {Box} from "@material-ui/core";
+// @ts-ignore
+import defaultImage from "../../images/default-image.jpg";
+import Dropzone from "react-dropzone";
 
-type ProfileImageProps = {
-}
+type ProfileImageProps = {}
 
 type ProfileImageState = {
     selectedImage: File | null;
@@ -36,42 +38,67 @@ class ProfileImage extends Component<ProfileImageProps, ProfileImageState> {
         }
     }
 
+    handleDropImage = async (e: React.DragEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const droppedFiles = e.dataTransfer.files;
+
+        const state = this.context as EmployeeState;
+
+        const storedEmployeeData = localStorage.getItem("employee");
+        const employeeData = storedEmployeeData ? JSON.parse(storedEmployeeData) : {};
+
+        if(droppedFiles && droppedFiles.length > 0) {
+            const imageFile = droppedFiles[0];
+            this.setState({
+                selectedImage: imageFile,
+            });
+        }
+    };
+
     render() {
         const storedEmployeeData = localStorage.getItem('employee');
         const employeeData = storedEmployeeData ? JSON.parse(storedEmployeeData) : {};
 
         return (
-            <Box style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                marginTop: '20px'
-            }}
+            <Box
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    marginTop: '20px'
+                }}
             >
-                {this.state.selectedImage ? (
-                    <img
-                        src={URL.createObjectURL(this.state.selectedImage)}
-                        alt="새 이미지"
-                        style={{
-                            width: '200px',
-                            height: '250px',
-                            marginTop: '10px',
-                            marginBottom: "10px",
-                            borderRadius: '20%'
-                        }}/>
-                ) : employeeData.employeeNo !== 0 ? (
-                    <img src={('http://localhost:8080/employees/'+employeeData.employeeNo+'/image')}
-                         style={{
-                             maxWidth: '200px',
-                             maxHeight: '250px',
-                             marginTop: '10px',
-                             marginBottom: "10px",
-                             borderRadius: '20%'
-                         }}/>
-                ) : (
-                    <div> 이미지 없음 </div>
-                )}
+                <div onDrop={this.handleDropImage}
+                     onDragOver={(e) => e.preventDefault()}>
+                    {this.state.selectedImage ? (
+                        <img
+                            src={URL.createObjectURL(this.state.selectedImage)}
+                            alt="새 이미지"
+                            style={{
+                                width: '200px',
+                                height: '250px',
+                                marginTop: '10px',
+                                marginBottom: "10px",
+                                borderRadius: '20%'
+                            }}/>
+                    ) : employeeData.employeeNo !== 0 ? (
+                        <img src={('http://localhost:8080/employees/' + employeeData.employeeNo + '/image')}
+                             style={{
+                                 maxWidth: '200px',
+                                 maxHeight: '250px',
+                                 marginTop: '10px',
+                                 marginBottom: "10px",
+                                 borderRadius: '20%'
+                             }}
+                             onError={(e) => {
+                                 e.currentTarget.src = defaultImage;
+                             }}
+                        />
+                    ) : (
+                        <div> 이미지 없음 </div>
+                    )}
+                </div>
                 <button onClick={() => document.getElementById('fileInput')?.click()}
                         style={{
                             backgroundColor: '#546ae8',
